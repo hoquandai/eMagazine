01ead7367c38c777f0306d0277cacb65934ae808
HPLake <hoquandai@gmail.com>
2019-05-12 22:28:32 +0700

    upload admin subscriber reader routes

--- app.js
+++ app.js
@@ -29,6 +29,32 @@ app.get('/', (req, res) => {
     })
 });
 
+/// READER
+app.use('/news', require('./routes/reader/news.route'));
+app.use('/chinhtri', require('./routes/reader/chinhtri.route'));
+app.use('/top', require('./routes/reader/top.route'));
+app.use('/giaoduc', require('./routes/reader/giaoduc.route'));
+app.use('/xahoi', require('./routes/reader/xahoi.route'));
+app.use('/phapluat', require('./routes/reader/phapluat.route'));
+app.use('/nongsan', require('./routes/reader/kinhdoanh/nongsan.route'));
+app.use('/haisan', require('./routes/reader/kinhdoanh/haisan.route'));
+app.use('/subscriber', require('./routes/subscriber/index.route'));
+
+/// INFO
+app.use('/about', require('./routes/info/about.route'));
+app.use('/privacy', require('./routes/info/privacy.route'));
+app.use('/contact', require('./routes/info/contact.route'));
+app.use('/login', require('./routes/info/login.route'));
+app.use('/signup', require('./routes/info/signup.route'));
+
+/// ADMIN
+app.use('/cate_management', require('./routes/admin/categories.route'));
+app.use('/detail_management', require('./routes/admin/detail.route'));
+app.use('/post_management', require('./routes/admin/posts.route'));
+app.use('/tag_management', require('./routes/admin/tag.route'));
+app.use('/user_management', require('./routes/admin/users.route'));
+
+
 app.listen(3002, () => {
     console.log('server is running at http://localhost:3002');
 })
