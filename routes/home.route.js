ac604059660f650383068aafa2ff75729c003d2d
HPLake <hoquandai@gmail.com>
2019-05-24 08:28:32 +0700

    add home route

---
+++ routes/home.route.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var categoryModel = require('../models/category.model')
+var router = express.Router();
+
+
+router.get('/', (req, res) => {
+    var p = categoryModel.all();
+    p.then(rows => {
+        console.log(rows);
+        res.render('home', {
+            categories: rows
+        });
+    }).catch(err => {
+        console.log(err);
+    });
+})
+
+module.exports = router;
